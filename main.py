783c3d9fcbda47e27e918cdaf62ce130a63ca3f9
Oleksii Petrashov <oleksii.petrashov@gmail.com>
2023-06-28 04:41:46 +0300

    Data files creation report

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ def main():
 
     os.makedirs(pathlib.Path('data'), exist_ok=True)
 
-    with open(pathlib.Path('data/authors.json'), 'w') as authors_fh:
+    with open(file_name := pathlib.Path('data/authors.json'), 'w') as authors_fh:
         authors_fh.write(parser.authors)
+    print(file_name, 'has been written.')
 
-    with open(pathlib.Path('data/quotes.json'), 'w') as quotes_fh:
+    with open(file_name := pathlib.Path('data/quotes.json'), 'w') as quotes_fh:
         quotes_fh.write(parser.quotes)
+    print(file_name, 'has been written.')
 
 
 if __name__ == "__main__":
